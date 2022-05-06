b5ac9aebccbe524dad74ad8ea3decbc9d310b9d1
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-06 16:02:41 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # destroy all datas
 Prestation.destroy_all
 
@@ -57,5 +49,4 @@ presta = Prestation.new(title: "Zone de stockage",
             description: ""
 )
 presta.save!
-
 puts "Seeds terminated !"
