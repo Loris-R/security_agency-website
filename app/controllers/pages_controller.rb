db05d1ad0993f1579434889dcc044ec530303e63
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-10 16:09:59 +0200

    seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,9 @@ class PagesController < ApplicationController
     @header = "header-about.jpg"
   end
 
-  def secteurs
+  def expertise
     @header = "header-activities.jpg"
+    @activities = Activity.all
   end
 
   def prestations
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :contact_forms, only: %i(new create)
 
   get 'qui-sommes-nous', to: 'pages#about'
-  get 'secteurs-dactivites', to: 'pages#secteurs'
+  get 'expertise', to: 'pages#expertise'
   get 'prestations', to: 'pages#prestations'
   get 'contact', to: 'pages#contact'
 end
