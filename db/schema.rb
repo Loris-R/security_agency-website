f5f058d684d959d077a8eb3fa216344bb3ad31b9
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-10 17:25:04 +0200

    deleted table prestation, no need

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_144621) do
+ActiveRecord::Schema.define(version: 2022_05_10_152353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,14 +41,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_144621) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "activity_id"
     t.index ["activity_id"], name: "index_images_on_activity_id"
-  end
-
-  create_table "prestations", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "subtitle"
   end
 
   create_table "quotes", force: :cascade do |t|
