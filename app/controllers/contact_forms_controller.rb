e079a948b798a21619b42b27acfeb6bc98f3c116
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-11 14:33:35 +0200

    quote saved in db

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,4 +1,17 @@
 class ContactFormsController < ApplicationController
   def create
+    @contact_form = Contact_form.new(contact_form_params)
+    if @contact_form.save
+      flash[:alert] = "Votre demande de devis a été envoyée, merci !"
+    else
+      render 'contact'
+      flash[:alert] = "Il y a eu un problème, merci de nous contacter par téléphone"
+    end
+  end
+
+  private
+
+  def contact_form_params
+    params.require(:contact_form).permit(:first_name, :last_name, :email, :phone_number, :is_application, :message, :rgpd)
   end
 end
