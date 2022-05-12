39449de8096834fe903163310320a53a6e4dfbf3
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-12 11:01:09 +0200

    contact_form works, need flash alert

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,11 +1,9 @@
 class ContactFormsController < ApplicationController
   def create
-    @contact_form = Contact_form.new(contact_form_params)
+    @contact_form = ContactForm.new(contact_form_params)
     if @contact_form.save
-      flash[:alert] = "Votre demande de devis a été envoyée, merci !"
     else
       render 'contact'
-      flash[:alert] = "Il y a eu un problème, merci de nous contacter par téléphone"
     end
   end
 
