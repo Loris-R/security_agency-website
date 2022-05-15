96f6fb958115d10128e15da6cc58ab14edec0115
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-15 13:04:20 +0200

    quote form with flashes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,18 @@
 class QuotesController < ApplicationController
-  def new
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save!
+      redirect_to root_path
+      flash[:notice] = 'Votre demande de devis à bien été envoyé, nous revenons vers vous rapidement !'
+    else
+      redirect_to root_path
+      flash.now[:alert] = 'Il y a eu un petit problème, appelez nous par téléphone le temps qu\'on règle ça !'
+    end
   end
 
-  def create
+  private
+
+  def quote_params
+    params.require(:quote).permit(:society_name, :name, :prestation_type, :work_description, :email)
   end
 end
