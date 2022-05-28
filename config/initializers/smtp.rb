901ea086a776e4e19745fb4a6cb142d2d51ff614
Loris Reynaud <loris.reynaud@gmail.com>
2022-05-28 16:06:03 +0200

    ok

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.ionos.fr",
   port: 587,
   domain: 'ionos.fr',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  user_name: ENV['EMAIL_ADDRESS'],
+  password: ENV['EMAIL_APP_PASSWORD'],
   authentication: :plain,
   enable_starttls_auto: true
 }
