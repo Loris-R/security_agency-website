b6b6b55517badf409be518da695ac1b6328660d1
Loris Reynaud <loris.reynaud@gmail.com>
2022-06-01 13:20:54 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "212.227.197.131" }
+  config.action_mailer.default_url_options = { host: "lap-securite.com" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
