bab4ce2943e8218f2756a1cacd4f0108d25225d9
Loris Reynaud <loris.reynaud@gmail.com>
2022-06-01 14:35:30 +0200

    alert for rgpd false

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,14 +1,21 @@
 class ContactFormsController < ApplicationController
   def create
     @contact_form = ContactForm.new(contact_form_params)
-    if @contact_form.save!
-      ContactFormMailer.with(contact_form: @contact_form).new_contact.deliver_now
-      redirect_to root_path
-      flash[:notice] = 'Votre message à bien été envoyé, merci !'
+    if @contact_form.save
+        ContactFormMailer.with(contact_form: @contact_form).new_contact.deliver_now
+        redirect_to root_path
+        flash[:notice] = 'Votre message à bien été envoyé, merci !'
+    elsif @contact_form.rgpd == false
+      respond_to do |format|
+        format.js
+      end
     else
+      raise
       redirect_to root_path
-      flash.now[:alert] = 'Il y a eu un petit problème, appelez nous par téléphone le temps qu\'on règle ça !'
+      flash[:alert] = 'Il y a eu un petit problème, appelez nous par téléphone le temps qu\'on règle ça !'
     end
+
+    
   end
 
   private
