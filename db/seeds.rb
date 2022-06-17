adc1b27f0c9dd3717ae2bc22843b61a238c565bd
Loris Reynaud <loris.reynaud@gmail.com>
2022-06-17 13:09:08 +0200

    text

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,7 @@ activity = Activity.new(title: "Bâtiment",
             subtitle: "Bailleurs sociaux, syndics",
             description: "Notre professionnalisme et le sérieux de nos agents nous ont spécialisé dans l’application de la tranquillité résidentielle 
                           au sein des copropriétés, des bailleurs sociaux…\n
-                          Ce secteur d’activité couvre tout le territoire national : contrôle d’accès selon les spécificités de votre copropriété, 
-                          gestion des places de parking, des poubelles, des déchets, surveillance piscine, sécurisation des lieux pour éviter leur dégradation, etc."
+                          Ce secteur d’activité couvre tout le territoire national : contrôle d’accès selon les spécificités de votre copropriété, gestion des places de parking, des poubelles, des déchets, surveillance piscine, sécurisation des lieux pour éviter leur dégradation, etc."
 )
 activity.save!
 
@@ -35,8 +34,7 @@ puts "creating Résidence activity..."
 activity = Activity.new(title: "Résidence",
             subtitle: "",
             description: "L’agent de sécurité appliquera les consignes que vous nous aurez communiquées.\n
-                          Le plus souvent : protection des biens et des lieux pour éviter toute intrusion, dégradation, vol. En cas de problème, 
-                          il pourra intervenir très rapidement pour preserver la sécurité des zones sous surveillance."
+                          Le plus souvent : protection des biens et des lieux pour éviter toute intrusion, dégradation, vol. En cas de problème, il pourra intervenir très rapidement pour preserver la sécurité des zones sous surveillance."
 )
 activity.save!
 
@@ -63,8 +61,7 @@ puts "creating hotel activity..."
 activity = Activity.new(title: "Hôtel",
             subtitle: "Hôtellerie, ERP",
             description: "L’agent de sécurité appliquera les consignes que vous nous aurez communiquées.\n
-                          Le plus souvent : ouverture fermeture des portes, protection des lieux, gestion des déplacements des clients pour un bon 
-                          déroulement, ainsi il pourra aussi donner des renseignements au client…\n
+                          Le plus souvent : ouverture fermeture des portes, protection des lieux, gestion des déplacements des clients pour un bon déroulement, ainsi il pourra aussi donner des renseignements au client…\n
                           Gestion discrète et efficace de la sécurité de vos commerces, supermarchés, hypermarchés, hôtels, restaurants, hôpitaux…"
 )
 activity.save!
@@ -119,9 +116,7 @@ puts "creating BTP activity..."
 activity = Activity.new(title: "BTP",
             subtitle: "Surveillance des chantiers",
             description: "L’agent de sécurité appliquera les consignes que vous nous aurez communiquées.\n
-                          Notamment : contrôle d’accès des véhicules et des personnes afin de s’assurer que des individus non
-                          autorisés ne puissent pénétrer sur les chantiers, ainsi que la surveillance sur site pour prévenir de vols ou
-                          dégradations…"
+                          Notamment : contrôle d’accès des véhicules et des personnes afin de s’assurer que des individus non autorisés ne puissent pénétrer sur les chantiers, ainsi que la surveillance sur site pour prévenir de vols ou dégradations…"
 )
 activity.save!
 
